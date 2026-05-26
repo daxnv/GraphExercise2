1300de435115336ea202f87036553109f8f976bb
daxnv <da.nv@web.de>
2026-05-26 15:26:39 +0200

    Improve comments

--- main.cpp
+++ main.cpp
@@ -11,27 +11,21 @@
 
 using namespace std;
 
-/*
-Diese Funktion verwendet eine sehr abgespeckte Graphenklasse. Um sie zusammen mit
-der bereitgestellten Graphenklasse zu verwenden, kann man dort zum Beispiel
-eine Methode get_edge_list() implementieren. Im Kontext dieser Aufgabe ist es aber
-sinnlos, die in der Eingabedatei gegebene Struktur hin- und herzuwandeln.
-*/
-vector<Edge> boruvka(Graph &&graph) {
+vector<Edge> boruvka(Graph &&graph) {  // Boruvka's algorithm
     using CompId = int;
     using Component = struct {
         vector<NodeId> nodes;
         optional<Edge> min_edge;
     };
 
-    vector component_of = vector<CompId>(graph.num_nodes);  // List to get number of component given node id
+    vector component_of = vector<CompId>(graph.num_nodes);  // List to save component number of each node
     iota(component_of.begin(), component_of.end(), 0);
 
     unordered_map components = unordered_map<CompId, Component>(graph.num_nodes);  // List to keep track of remaining components
     for (NodeId node_id = 0; node_id < graph.num_nodes; ++node_id)
-        components.emplace(node_id, Component{.nodes = vector{node_id}, .min_edge = optional<Edge>()});  // Create singleton components with not pointing to an edge yet
+        components.emplace(node_id, Component{.nodes = vector{node_id}, .min_edge = optional<Edge>()});  // Create singleton components for each node
 
-    vector mst_edges = std::vector<Edge>();  // List of edges of the MST to return
+    vector mst_edges = std::vector<Edge>();  // Edges of the minimal spanning tree returned at the end
 
     while (components.size() > 1) {
         erase_if(graph.edges, [&](const Edge &edge) { return component_of[edge.end_nodes[0]] == component_of[edge.end_nodes[1]]; });  // Remove edges with nodes in same component for faster iteration
@@ -49,46 +43,39 @@ vector<Edge> boruvka(Graph &&graph) {
         }
 
         for (auto component = components.begin(); component != components.end();) {  // Merge components by their minimal edges
-            if (!component->second.min_edge.has_value()) {                           // No value means component was already merged with the desired one
+            if (!component->second.min_edge.has_value()) {                           // No value means component was already merged in a previous iteration of this loop
                 ++component;
                 continue;
             }
 
             mst_edges.push_back(*component->second.min_edge);  // Add edge to the edges of MST
 
-            auto other =  // Determine, which end node is not in component and get its component
+            auto other =  // Determine the other component, where the minimal outgoing edge leads to
                 (component_of[component->second.min_edge->end_nodes[1]] == component->first) ? components.find(component_of[component->second.min_edge->end_nodes[0]])
                                                                                              : components.find(component_of[component->second.min_edge->end_nodes[1]]);
-            bool less = component->second.nodes.size() < other->second.nodes.size();  // Compare sizes of component and other
+            bool less = component->second.nodes.size() < other->second.nodes.size();  // Compare sizes of component and other measured as number of nodes
             const auto &smaller = less ? component : other;
             const auto &bigger = !less ? component : other;
 
-            for (NodeId node_id : smaller->second.nodes) {  // Relabel nodes in smaller. They now belong to bigger
+            for (NodeId node_id : smaller->second.nodes) {  // Relabel nodes in smaller component to now belong to the bigger one
                 component_of[node_id] = bigger->first;
             }
             bigger->second.nodes.insert(bigger->second.nodes.end(), smaller->second.nodes.begin(), smaller->second.nodes.end());  // Add nodes from smaller to bigger
 
-            if (less) {  // If component was smaller, it was absorbed. Re-validate iterator
+            if (less) {  // If component was smaller and thus absorbed, then re-validate iterator
                 component = components.erase(component);
-            } else {  // If other was absorbed but had its minimal outgoing edge to a third component, remember this so we can merge with it later
+            } else {  // If other was smaller and thus absorbed, then remember minimal outgoing edge to also perform a merge on that one
                 component->second.min_edge = other->second.min_edge;
-                components.erase(other);  // Relies on the commented out test snippet below
+                components.erase(other);
             }
 
-            if (bigger->second.min_edge.has_value() && component_of[bigger->second.min_edge->end_nodes[0]] == component_of[bigger->second.min_edge->end_nodes[1]])  // If this merge was desired by bigger too, set status to "done" by setting the option to none
+            if (bigger->second.min_edge.has_value() && component_of[bigger->second.min_edge->end_nodes[0]] == component_of[bigger->second.min_edge->end_nodes[1]])  // If both components requested this merge, remove the link also from the other component as nothing is left to do
                 bigger->second.min_edge.reset();
         }
     }
 
     return mst_edges;
 }
-
-/* test *
-unordered_map alph = unordered_map<int, char>{{2, 'b'}, {5, 'e'}};
-auto it = alph.begin();
-alph.erase(++alph.begin());
-cout << (++it == alph.end() ? "pass" : "fail") << endl;
-*/
 
 int main(int argc, char *argv[]) {
     if (argc > 1) {
